0f6c10c78d253d11d0bd7f9fe8e6ccd171769e9f
Brandon Geren <brandongeren@gmail.com>
2017-02-10 23:53:07 -0600

    Migrate data to a model

---
+++ app/models/poem.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	id: DS.attr(),
+	title: DS.attr(),
+	author: DS.attr(),
+	dateSubmitted: DS.attr(),
+	text: DS.attr(),
+});
--- app/routes/poems.js
+++ app/routes/poems.js
@@ -2,18 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [{
-    	id: 1,
-    	title: 'Overthoughts',
-    	author: 'Brandon Geren',
-    	dateSubmitted: '', // automatically stored when poem submitted
-    	text: 'Am I using it too much? ...'
-    }, {
-    	id: 2,
-    	title: 'Laundry baskets',
-    	author: 'Brandon Geren',
-    	dateSubmitted: '', // stored programmatically into the database upon submission
-    	text: 'Why do laundry baskets have holes in the sides? ...'
-    }];
+    return this.get('store').findAll('rental');
   }
 });
